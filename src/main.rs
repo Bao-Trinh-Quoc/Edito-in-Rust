2bacdce8f1aac166d1b778a1fc2f5fe869cd314d
Bao-Trinh-Quoc <bao.trinhquoc@hcmut.edu.vn>
2025-07-21 17:32:00 +0700

    refactor: Make code more idiotmatic

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
-use std::io::{self, Read};
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+mod editor;
+
+use editor::Editor;
 
 fn main() {
     // println!("Hello, edito!");
-    // Keep unwrap here because we want to panic if enabling raw mode fails.
-    enable_raw_mode().unwrap();
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-              
-                if c.is_control() {
-                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-                } else {
-                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-                }
+    let editor = Editor::default();
 
-                if c == 'q' {
-                    println!("Exiting...");
-                    break;
-                }
-            },
-            Err(e) => println!("Error reading input: {}", e),
-        }
-    }
-    disable_raw_mode().unwrap();
+    editor.run();
 }
