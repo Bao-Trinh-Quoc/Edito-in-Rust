c6cf9a65f80e5e4dd9af2af0f7f326ab1951fab4
Bao-Trinh-Quoc <bao.trinhquoc@hcmut.edu.vn>
2025-07-23 16:14:39 +0700

    refactor: ultilize crossterm instead of std

--- src/editor.rs
+++ src/editor.rs
@@ -1,37 +1,33 @@
-use std::io::{self, Read};
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+use crossterm::event::{Event::Key, KeyCode::Char, read};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 pub struct Editor {}
 
 impl Editor {
-   pub fn default() -> Self {
-       
-        Editor{}
-   }  
-    
-   pub fn run(&self) {
-        enable_raw_mode().unwrap();
-        for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-            
-                if c.is_control() {
-                        println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-                    } else {
-                        println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-                    }
+    pub fn default() -> Self {
+        Editor {}
+    }
 
-                    if c == 'q' {
-                        println!("Exiting...");
-                        break;
+    pub fn run(&self) {
+        enable_raw_mode().unwrap();
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{event:?} \r");
+
+                    match event.code {
+                        Char(c) => {
+                            if c == 'q' {
+                                break;
+                            }
+                        }
+                        _ => (),
                     }
-                },
-                Err(e) => println!("Error reading input: {}", e),
+                }
+                Err(e) => println!("Error reading input: {e}"),
+                _ => (),
             }
         }
         disable_raw_mode().unwrap();
-
     }
-
 }
