f3295638363639ad14085b944c510e39950dc138
Bao-Trinh-Quoc <bao.trinhquoc@hcmut.edu.vn>
2025-08-01 14:35:04 +0700

    refactor: separate terminal module

--- src/editor.rs
+++ src/editor.rs
@@ -1,52 +1,34 @@
-use crossterm::cursor::MoveTo;
 use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
-use crossterm::execute;
-use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode};
-use std::io::stdout;
+mod terminal;
+use terminal::Terminal;
 
 pub struct Editor {
     should_quit: bool,
 }
 
 impl Editor {
-    pub fn default() -> Self {
-        Editor { should_quit: false }
+    pub const fn default() -> Self {
+        Self { should_quit: false }
     }
 
     pub fn run(&mut self) {
-        Self::initialize().unwrap();
+        Terminal::init().unwrap();
         let result = self.repl();
-        Self::terminate().unwrap();
+        Terminal::terminate().unwrap();
         result.unwrap();
     }
 
     pub fn repl(&mut self) -> Result<(), std::io::Error> {
-        self.draw_rows();
         loop {
-            let event = read()?;
-            self.evalute_event(&event);
             self.refresh_screen()?;
 
             if self.should_quit {
                 break;
             }
+            let event = read()?;
+            self.evalute_event(&event);
         }
         Ok(())
-    }
-
-    fn initialize() -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
-        Self::clear_screen()
-    }
-
-    fn terminate() -> Result<(), std::io::Error> {
-        disable_raw_mode()
-    }
-
-    fn clear_screen() -> Result<(), std::io::Error> {
-        let mut stdout = stdout();
-        execute!(stdout, MoveTo(0, 0)).unwrap();
-        execute!(stdout, Clear(ClearType::All))
     }
 
     fn evalute_event(&mut self, event: &Event) {
@@ -62,22 +44,24 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         if self.should_quit {
-            Self::clear_screen()?;
+            Terminal::clear_screen()?;
             println!("Ending Edito \r\n");
+        } else {
+            Self::draw_rows()?;
+            Terminal::move_cursor_to(0, 0)?;
         }
         Ok(())
     }
 
-    fn draw_rows(&mut self) -> Result<(), std::io::Error> {
-        let terminal_size_y = crossterm::terminal::size().unwrap().1;
-        let mut stdout = stdout();
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let terminal_size_y = Terminal::size()?.1;
         // Draw ~ in every row
-        for i in 0..terminal_size_y {
-            execute!(stdout, MoveTo(0, i)).unwrap();
-            println!("~");
+        for current_row in 0..terminal_size_y {
+            print!("~");
+            if current_row + 1 < terminal_size_y {
+                print!("\r\n");
+            }
         }
-        // then move back the cursor to the top-left
-        execute!(stdout, MoveTo(0, 0)).unwrap();
         Ok(())
     }
 }
